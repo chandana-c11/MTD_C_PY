6a191f4bf3924cd1af50deab4891df4970512b92
chandana-c11 <chandanac0111@gmail.com>
2025-03-07 08:47:02 +0530

    notes files to be added

---
+++ day1/p1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int num1 = 0;
+    long 
+    scanf("%d", &inutmum);
+    if(inutmum % 2 == 0)
+        printf("%d is Even", inutmum);
+    else
+        printf("%d is not Even", inutmum);
+    return 0;
+    
+}
